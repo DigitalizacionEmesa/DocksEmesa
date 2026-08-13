19c6f8e1a4dc929aab8696ce15766a4053faa680
jmdbaru <barutell98@gmail.com>
2026-08-13 13:35:44 +0200

    Netlify-Test-3-Flask-Funtion

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,16 +1,4 @@
-import os
-import sys
-
-# Añadir la raíz del proyecto al path de Python
-ROOT_DIR = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "../..")
-)
-
-if ROOT_DIR not in sys.path:
-    sys.path.insert(0, ROOT_DIR)
-
 from app import app
-
 from serverless_wsgi import handle_request
 
 
